bf97e683f0c9feb2a40118ef7c931afe0e48b7a2
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-06 18:01:28 +0200

    update code

--- src/components/MusicGame/MusicGame.tsx
+++ src/components/MusicGame/MusicGame.tsx
@@ -3,7 +3,7 @@ import Piano from "../Piano/Piano";
 
 export default function MusicGame() {
     return (
-        <div className="flex flex-col relative justify-between items-center w-full lg:w-3/5 h-3/5 backdrop-blur-2xl rounded-3xl border border-yellow-400">
+        <div className="flex flex-col relative justify-between items-center w-full lg:w-3/5 h-[70%] backdrop-blur-2xl rounded-3xl border border-yellow-400">
             {/* Instructions */}
             <section className="flex flex-col absolute left-0 bg-white/20 w-fit px-4 py-3 rounded-xl text-sm">
                 <div className='flex flex-col items-start gap-1'>
@@ -20,12 +20,12 @@ export default function MusicGame() {
             </section>
 
             {/* Button Start Game */}
-            <button id='start' className='px-4 py-2 font-semibold mt-5 bg-yellow-400 rounded-lg w-fit hover:bg-yellow-600 transition-all ease-in-out duration-300'>
+            <button id='start' className='px-4 py-2 font-semibold mt-3 bg-yellow-400 rounded-lg w-fit hover:bg-yellow-600 transition-all ease-in-out duration-300'>
                 <span>Start Game</span>
             </button>
 
             {/* Title */}
-            <section className="flex justify-center items-center gap-3 sm:gap-5 mt-10 mb-4">
+            <section className="flex justify-center items-center gap-3 sm:gap-5 mt-7 mb-4">
                 <img src="/src/assets/images/icon-music.png" className="w-8 sm:w-10 lg:w-12 xl:w-14" alt="Note of music" />
                 <h1 className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-medium text-white font-sans">Virtual Piano Game</h1>
                 <img src="/src/assets/images/icon-music.png" className="w-8 sm:w-10 lg:w-12 xl:w-14" alt="Note of music" />
--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -3,7 +3,7 @@ import { keys, playNote, handlePressKey } from "../../utils/tone";
 
 export default function Piano() {
     return (
-        <div className="bg-white w-full h-60 flex justify-center items-center">
+        <div className="bg-white w-full lg:h-[22rem] flex justify-center items-center">
             Piano
         </div>
     );

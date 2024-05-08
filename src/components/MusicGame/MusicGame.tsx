febc8d84fc82857da9ee3d0ef995dd0a245c3698
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-08 20:16:55 +0200

    chore: Update CSS styles for piano keys and background gradient

--- src/components/MusicGame/MusicGame.tsx
+++ src/components/MusicGame/MusicGame.tsx
@@ -1,78 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Piano from "../Piano/Piano";
 import { keys, playNote } from "../../utils/tone";
 
 export default function MusicGame() {
 
-    // Estado para almacenar el tiempo
+    // Lógica del juego Virtual Piano Game
+
+    // 1. Generar una secuencia aleatoria 4 teclas (Notas musicales)
+    // 2. Reproducir la secuencia de teclas generada
+    // 3. Esperar a que el usuario presione las teclas correctas en el mismo orden
+    // 4. Cada ronda completada con éxito aumenta la dificultad del juego (Velocidad de reproducción de la secuencia)
+    // 5. Si el usuario presiona la tecla correcta, aumentamos la puntuación a 50 puntos y continuamos con la siguiente ronda (Secuencia anterior + 1 tecla)
+    // 6. Si el usuario presiona una tecla incorrecta, detenemos el juego, mostramos un modal con la puntuación final junto con un botón para cerrar el modal
+
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
+    const timeInterval = useRef<number | undefined>();
 
     let sequence: string[] = [];
-    let intervalId;
 
     useEffect(() => {
-        // Función para actualizar el tiempo
-        const updateTime = () => {
+        function updateTime() {
             setTime((prevTime) => prevTime + 1);
-        };
-
-        // Configura un intervalo para actualizar el tiempo cada segundo solo si el juego está en marcha
-        if (isRunning) {
-            intervalId = setInterval(updateTime, 1000);
         }
-
-        // Limpia el intervalo cuando el componente se desmonte o se detenga el juego
-        return () => clearInterval(intervalId);
+        if (isRunning) {
+            timeInterval.current = window.setInterval(updateTime, 1000);
+        }
+        return () => clearInterval(timeInterval.current);
     }, [isRunning]);
 
-
-    // Lógica del juego
-    // 1. Generar una secuencia aleatoria de teclas
-    // 2. Reproducir la secuencia de teclas
-    // 3. Esperar a que el usuario presione las teclas correctas
-    // 4. Si el usuario presiona la tecla correcta, aumentar la puntuación y continuar con la siguiente tecla
-    // 5. Si el usuario presiona una tecla incorrecta, detener el juego y mostrar la puntuación final
-
-    // Función para iniciar el juego
-    const startGame = () => {
-        console.log("Start Game");
-
-        setIsRunning(true);
-        setTime(0);
-
-        // Generar una secuencia aleatoria de teclas
-        sequence = generateRandomSequence(4);
-        console.log("Sequence:", sequence);
-
-        setInterval(() => {
-            sequence.push(...generateRandomSequence(1));
-            console.log("Sequence:", sequence);
-            playSequence(sequence);
-        }, 4000 + sequence.length * 1000);
-
-        playSequence(sequence);
-    };
-
     // Función para generar una secuencia aleatoria de teclas
-    const generateRandomSequence = (length: number) => {
-        // Ejemplo de cómo generar una secuencia aleatoria de teclas
+    function generateRandomSequence(length: number) {
         const notes = keys.map((key) => key.key);
         const sequence: string[] = [];
         for (let i = 1; i <= length; i++) {
             const randomIndex = Math.floor(Math.random() * notes.length);
             sequence.push(notes[randomIndex]);
         }
-        console.log("New note:", sequence);    
-        return sequence;    
+        return sequence;
     }
 
-    // Ejemplo de cómo reproducir la secuencia de teclas
+    // Función para reproducir una secuencia de teclas
     async function playSequence(sequence: string[]) {
         for (const key of sequence) {
-            const pianoKey = keys.find(k => k.key === key);
+            const pianoKey = keys.find((k) => k.key === key);
             if (pianoKey) {
-                await new Promise(resolve => {
+                await new Promise((resolve) => {
                     setTimeout(() => {
                         playNote(pianoKey.note);
                         resolve(null);
@@ -82,8 +55,26 @@ export default function MusicGame() {
         }
     }
 
+    // Función para iniciar el juego
+    function startGame() {
+
+        // Reiniciar el tiempo y la secuencia
+        setIsRunning(true);
+        setTime(0);
+
+        // Generar una secuencia aleatoria de teclas
+        sequence = generateRandomSequence(4);
+
+        setInterval(() => {
+            sequence.push(...generateRandomSequence(1));
+            playSequence(sequence);
+        }, 4000 + sequence.length * 1000);
+
+        playSequence(sequence);
+    }
+
     return (
-        <section className="flex flex-col relative items-center w-full lg:w-3/5 h-[37rem] backdrop-blur-2xl rounded-3xl border border-yellow-400">
+        <section className="flex flex-col relative items-center w-full lg:w-3/5 h-[37rem] backdrop-blur-xl rounded-3xl border border-yellow-400">
             {/* Instructions */}
             <article className="flex flex-col absolute left-0 bg-white/20 w-fit px-4 py-3 rounded-xl text-sm">
                 <div className='flex flex-col items-start gap-1'>
--- src/utils/tone.ts
+++ src/utils/tone.ts
@@ -2,33 +2,32 @@ import * as Tone from "tone";
 import { PianoKey } from "../interfaces";
 
 export const keys: PianoKey[] = [
-    { note: "C4", key: "A", color: "white" },
+    { note: "C4",  key: "A", color: "white" },
     { note: "Db4", key: "W", color: "black" },
-    { note: "D4", key: "S", color: "white" },
+    { note: "D4",  key: "S", color: "white" },
     { note: "Eb4", key: "E", color: "black" },
-    { note: "E4", key: "D", color: "white" },
-    { note: "F4", key: "F", color: "white" },
+    { note: "E4",  key: "D", color: "white" },
+    { note: "F4",  key: "F", color: "white" },
     { note: "Gb4", key: "T", color: "black" },
-    { note: "G4", key: "G", color: "white" },
+    { note: "G4",  key: "G", color: "white" },
     { note: "Ab4", key: "Y", color: "black" },
-    { note: "A4", key: "H", color: "white" },
+    { note: "A4",  key: "H", color: "white" },
     { note: "Bb4", key: "U", color: "black" },
-    { note: "B4", key: "J", color: "white" },
-    { note: "C5", key: "K", color: "white" },
+    { note: "B4",  key: "J", color: "white" },
+    { note: "C5",  key: "K", color: "white" },
 ];
 
 const synth = new Tone.Synth().toDestination();
 
 export function playNote(note: string): void {
     synth.triggerAttackRelease(note, "8n");
-
     const keyElement = document.getElementById(`key-${note}`);
     if (keyElement) {
         keyElement.classList.add(keys.find((item) => item.note === note)?.color === "white" ? "bg-white-hover" : "bg-black-hover");
 
         setTimeout(() => {
             keyElement.classList.remove(keys.find((item) => item.note === note)?.color === "white" ? "bg-white-hover" : "bg-black-hover");
-        }, 500);
+        }, 200);
     }
 }
 

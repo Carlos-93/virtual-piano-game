018ddea315de669937b7fef762db47ce1b85198f
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-27 12:54:19 +0200

    feat: Update routing in App component and main entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Logo from "./components/logo/Logo";
 import MusicGame from "./components/musicGame/MusicGame";
 
 export default function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={
-          <main className='h-screen grid place-items-center'>
-            <Logo />
-            <MusicGame />
-          </main>
-        } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={
+        <main className='h-screen grid place-items-center'>
+          <Logo />
+          <MusicGame />
+        </main>
+      } />
+    </Routes>
   );
 }
